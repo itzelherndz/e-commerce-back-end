f000d9b8ad11df6294f39fa75e874631a0831e7a
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-03-23 23:25:37 -0600

    updated model associations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  foreignKey: 'tag_id',
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,{
+  foreignKey: 'product_id',
+});
 
 module.exports = {
   Product,
